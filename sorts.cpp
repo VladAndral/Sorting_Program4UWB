61425e91d11930d077c91be2206fec6bd65964fb
Vladimir Andral (Jr.) <andralvladimir@gmail.com>
2024-11-21 11:33:46 -0800

    BubbleSort implemented

--- sorts.cpp
+++ sorts.cpp
@@ -1,12 +1,43 @@
 #include "sorts.h"
 #include <vector>
+#include <chrono>
+#include <iostream>
 
 using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+// using std::chrono;
+using std::srand;
 
+/* 
+    1. Develop, compile, and test locally
+    2. Push to git
+    3. Pull on LinuxLabs and run
+*/
 
 template <class T>
 void BubbleSort(vector<T> &list, T lowestValueItem, T highestValueItem) {
 
+    int size = list.size();
+    int start = 0;
+    int end = size-2;
+
+    while (end >= start) {
+        int currPos = start;
+        while (currPos <= end) {
+            T currentItem = list[currPos];
+            T nextItem = list[currPos+1];
+            if (currentItem > nextItem) {
+                T temp;
+                temp = currentItem;
+                list[currPos] = nextItem;
+                list[currPos+1] = temp;
+            }
+            currPos++;
+        }
+        end--;
+    }
 }
 
 template <class T>
@@ -35,5 +66,19 @@ void ShellSort(vector<T> &list, T lowestValueItem, T highestValueItem) {
 }
 
 int main(int argc, char *argv[]) {
+
+    srand(time(0));
+    int size = 7;
+    vector<int> testList;
+    for (int i = 0; i < size; i++) {
+        int toAdd = rand()%20;
+        testList.push_back(toAdd);
+    }
+
+    BubbleSort(testList, 0, 19);
+
+    for (int item : testList) {
+        cout << item << "  ";
+    }
 
 }
